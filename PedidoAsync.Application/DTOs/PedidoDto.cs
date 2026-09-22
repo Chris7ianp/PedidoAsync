7603d3b7a5980aec85792da92427e91a7e3b9276
Chris7ianp <christian.paulo.rj@gmail.com>
2026-09-22 15:22:10 -0300

    Implementação da camada Domain e Application.

---
+++ PedidoAsync.Application/DTOs/PedidoDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace PedidoAsync.Application
+{
+    public class PedidoDto
+    {
+        public Guid Id { get; set; }
+        public string Cliente { get; set; } = string.Empty;
+        public decimal Valor { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataProcessamento { get; set; }
+
+    }
+}
---
+++ PedidoAsync.Application/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,12 @@
+﻿using PedidoAsync.Domain.Entities;
+
+namespace PedidoAsync.Application
+{
+    public interface IPedidoRepository
+    {
+        Task AdicionarAsync(Pedido pedido);
+        Task<Pedido?> ObterPorIdAsync(Guid id);
+        Task<List<Pedido>> ObterTodosAsync();
+        Task AtualizarAsync(PedidoDto pedido);
+    }
+}
